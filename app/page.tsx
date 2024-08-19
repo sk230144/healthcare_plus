b2f4de4c3fae8d5057f43ced1ebf1938217bd501
Saurabh <risabht043@gmail.com>
2024-08-19 12:06:14 +0530

    finish otp ver, admin dash

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,19 @@
 import { Loader2 } from "lucide-react"
- 
+
 import { Button } from "@/components/ui/button"
 import Image from "next/image";
 import { PatientForm } from "@/components/forms/PatientForm";
 import Link from "next/link";
+import PassKeyModal from "@/components/PassKeyModal";
 
-export default function Home() {
+export default function Home({ searchParams }: SearchParamProps) {
+  const isAdmin = searchParams?.admin === "true";
+
   return (
-   <div className="flex h-screen max-h-screen">
-       <section className="remove-scrollbar container my-auto">
+    <div className="flex h-screen max-h-screen">
+
+      {isAdmin && <PassKeyModal />}
+      <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
           <Image
             src="/assets/icons/logo-full.svg"
@@ -18,7 +23,7 @@ export default function Home() {
             className="mb-12 h-10 w-fit"
           />
 
-          <PatientForm/>
+          <PatientForm />
 
           <div className="text-14-regular mt-20 flex justify-between">
             <p className="justify-items-end text-dark-600 xl:text-left">
@@ -38,6 +43,6 @@ export default function Home() {
         alt="patient"
         className="side-img max-w-[50%]"
       />
-   </div>
+    </div>
   );
 }
--- lib/actions/appointment.action.ts
+++ lib/actions/appointment.action.ts
@@ -1,8 +1,9 @@
 "use server";
 
-import { ID } from "node-appwrite";
+import { ID, Query } from "node-appwrite";
 import { APPOINTMENT_COLLECTION_ID, DATABASE_ID, databases } from "../appwrite.config";
 import { parseStringify } from "../utils";
+import { Appointment } from "./appwrite.types";
 
 
 
@@ -37,6 +38,53 @@ export const createAppointment = async (
     } catch (error) {
       console.error(
         "An error occurred while retrieving the existing patient:",
+        error
+      );
+    }
+  };
+
+  export const getRecentAppointmentList = async () => {
+    try {
+      const appointments = await databases.listDocuments(
+        DATABASE_ID!,
+        APPOINTMENT_COLLECTION_ID!,
+        [Query.orderDesc("$createdAt")]
+      );
+  
+      const initialCounts = {
+        scheduledCount: 0,
+        pendingCount: 0,
+        cancelledCount: 0,
+      };
+  
+      const counts = (appointments.documents as Appointment[]).reduce(
+        (acc, appointment) => {
+          switch (appointment.status) {
+            case "scheduled":
+              acc.scheduledCount++;
+              break;
+            case "pending":
+              acc.pendingCount++;
+              break;
+            case "cancelled":
+              acc.cancelledCount++;
+              break;
+          }
+          return acc;
+        },
+        initialCounts
+      );
+  
+      const data = {
+        totalCount: appointments.total,
+        ...counts,
+        documents: appointments.documents,
+      };
+  
+      return parseStringify(data);
+    } catch (error) {
+      console.error(
+        "An error occurred while retrieving the recent appointments:",
         error
       );
     }
